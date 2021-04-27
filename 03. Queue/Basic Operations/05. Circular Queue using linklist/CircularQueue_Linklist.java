61f98baed43e24ab6a117bd305456404d6f0b3e8
heyimvikash <heyimvikash@gmail.com>
2021-04-27 23:36:46 +0530

    Create CircularQueue_Linklist.java

---
+++ 03. Queue/Basic Operations/05. Circular Queue using linklist/CircularQueue_Linklist.java
@@ -0,0 +1,138 @@
+package stacksQueues.queue;
+
+import java.util.*;
+
+public class CircularQueue_Linklist {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		CircularQueue_LL q=new CircularQueue_LL();
+		boolean flag=true;
+		int valu=0;
+		while(flag) {
+			
+			System.out.println("1. Enqueue()");
+			System.out.println("2. Dequeue()");
+			System.out.println("3. Current size of queue");
+			System.out.println("4. peek()");
+			System.out.println("5. View queue");
+			System.out.println("6. Exit");
+			
+			System.out.println("Enter Choice");
+			int choice=sc.nextInt();
+			switch(choice) {
+			
+				case 1: System.out.println("Enter value");
+						valu=sc.nextInt();
+						q.enqueue(valu);
+				break;
+				
+				
+				case 2: System.out.println(q.dequeue());
+				break;
+				
+				
+				case 3: System.out.println(q.count());
+				break;
+				
+				case 4:System.out.println(q.peek());
+				break;
+				
+				case 5:q.viewQ();
+				break;
+				
+				case 6: flag=false;
+				break;
+				
+				default: System.out.println("invalid choice");
+			}//switch
+			System.out.println();
+		}//while
+
+	}
+
+}
+
+class CircularQueue_LL{
+	class Node{
+		private int data; 
+		private Node next;
+			
+		public Node(int data) {
+			this.data=data;next=null;
+		}
+	}
+	
+	private Node front;
+	private Node rear;
+	private int size;
+	
+	public CircularQueue_LL() {
+		front=null;
+		rear=null;
+		size=0;
+	}
+	
+	public boolean isEmpty() {
+		return (front==null && rear==null);
+	}
+	
+	public int count() {
+		return size;
+	}
+	
+	public void enqueue(int val) {
+		Node n=new Node(val);
+		if(isEmpty()) {
+			front=rear=n;
+		}
+		else {
+			rear.next=n;
+			rear=n;
+			n.next=front;
+		}
+		size++;
+	}
+	
+	public int dequeue(){
+		if(isEmpty()) {
+			System.out.println("Queue is empty");
+			return -999;
+		}
+		else if(front==rear) {
+			Node delD=front;
+			front=rear=null;
+			size--;
+			return delD.data;
+		}
+		else {
+			Node delD=front;
+			front=front.next;
+			rear.next=front;// for circular queue
+			delD.next=null;
+			size--;
+			return delD.data;
+		}
+	}
+	
+	public int peek(){
+		if(isEmpty()) {
+			System.out.println("Queue is Empty");
+			return -999;
+		}
+		return front.data;
+	}
+	
+	public void viewQ() {
+		if(isEmpty()) {
+			System.out.println("Queue is Empty");
+			return;
+		}
+		Node t=front;
+		 do{
+			System.out.print(t.data+" ");
+			t=t.next;
+		 }while(t!=front);
+		 System.out.print("-->"+t.data);
+	}
+}
