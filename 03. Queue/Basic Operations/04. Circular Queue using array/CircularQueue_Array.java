7bacb65e083e031de2352f807af5472d6f01165d
heyimvikash <heyimvikash@gmail.com>
2021-04-27 21:32:31 +0530

    new Q

---
+++ 03. Queue/Basic Operations/04. Circular Queue using array/CircularQueue_Array.java
@@ -0,0 +1,127 @@
+import java.util.*;
+
+public class CircularQueue_Array {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Size of Queue.");
+		int size=sc.nextInt();
+		CircularQueue q=new CircularQueue(size);
+		
+		boolean flag=true;
+		int valu=0;
+		while(flag) {
+			
+			System.out.println("1. Enqueue()");
+			System.out.println("2. Dequeue()");
+			System.out.println("3. Current size of queue");
+			System.out.println("4. peek()");
+			System.out.println("5. View queue");
+			System.out.println("6. Exit");
+						
+			System.out.println("Enter Choice");
+			int choice=sc.nextInt();
+			switch(choice) {
+			
+				case 1: System.out.println("Enter value");
+						valu=sc.nextInt();
+						q.enqueue(valu);
+				break;
+				
+				
+				case 2: System.out.println(q.dequeue());
+				break;
+				
+				
+				case 3: System.out.println(q.count());
+				break;
+				
+				case 4:System.out.println(q.peek());
+				break;
+				
+				case 5:q.viewQ();
+				break;
+				
+				case 6: flag=false;
+				break;
+								
+				default: System.out.println("invalid choice");
+			}//switch
+			System.out.println();
+		}//while
+
+	}
+
+}
+
+class CircularQueue{
+	private int front;
+	private int rear;
+	private int n;
+	private int cnt;
+	private int arr[];
+	
+	public CircularQueue(int size){
+		this.n=size;
+		arr=new int[n];
+		front=-1; rear=-1;cnt=0;
+	}
+	
+	public boolean isEmpty() {
+		return (rear==-1 && front==-1);
+	}
+	public boolean isFull() {
+		return ((rear+1)%n==front);
+	}
+	public int count() {
+		return cnt;
+	}
+	
+	public void enqueue(int val) {
+		if(isFull()) {
+			System.out.println("Queue  is full");
+			return;
+		}
+		else if(front==-1) {
+			front=rear=0;
+		}
+		else {
+			rear=(rear+1)%n;
+		}
+		arr[rear]=val;
+		cnt++;
+	}
+	public int dequeue() {
+		int x=0;
+		if(isEmpty()) {
+			System.out.println("Queue is empty");
+			return -999;
+		}
+		else if(rear==front) {
+			x = arr[front];
+		    front=rear=-1;
+		    return x;
+		}
+		else {
+			x=arr[front];
+			front=(front+1)%n;
+			return x;
+		}
+	}
+	public void viewQ() {
+		if(isEmpty()) {
+			System.out.println("Queue is empty");
+			return;
+		}
+		for(int i=0;i<n;i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+	public int peek() {
+		if(isEmpty()) {
+			System.out.println("Queue is empty");
+			return -999;
+		}
+		return arr[front];
+	}
+}
