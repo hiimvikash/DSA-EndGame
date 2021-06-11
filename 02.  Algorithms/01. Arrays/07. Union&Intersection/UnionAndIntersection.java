66985ea4fafa393787340ce06d754c2d0040c05e
heyimvikash <heyimvikash@gmail.com>
2021-06-11 12:24:10 +0530

    Create UnionAndIntersection.java

---
+++ 02.  Algorithms/01. Arrays/07. Union&Intersection/UnionAndIntersection.java
@@ -0,0 +1,32 @@
+package arrays;
+import java.util.*;
+
+public class UnionAndIntersection {
+
+	public static void main(String[] args) {
+		int arr1[] = { 7, 1, 5,5, 2, 3, 6,11,18 };
+        int arr2[] = { 3, 8, 6,5, 20, 7,18 };
+		
+		Set<Integer> union=new HashSet<>();
+		for(int val:arr1) {
+			union.add(val);
+		}
+		for(int val:arr2) {
+			union.add(val);
+		}
+		System.out.println("Union= "+union);
+		union.clear();// clearing union set to reuse for finding intersection
+		for(int val:arr1) {
+			union.add(val);
+		}
+		Set<Integer> intersection=new HashSet<>();
+		for(int val:arr2) {
+			intersection.add(val);
+		}
+		intersection.retainAll(union);
+		System.out.println("Intersection= "+intersection);
+
+
+	}
+
+}
