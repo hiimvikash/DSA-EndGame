26b67524cc42a392e6a327fa4ce9dd0f163be808
heyimvikash <heyimvikash@gmail.com>
2021-06-12 22:49:23 +0530

    changes

--- 02.  Algorithms/01. Arrays/02. MaxMin/MinMax.java
+++ 02.  Algorithms/01. Arrays/02. MaxMin/MinMax.java
@@ -1,28 +1,49 @@
 package arrays;
-
 public class MinMax {
 
 	public static void main(String[] args) {
-		//            0  1  2    3  4    5  6
-		int arr[]= {-900,23,67,1200,34,9800,0};
+		//            0  1  2    3  4    5  6    7
+		int arr[]= {-900,23,67,1200,34,9800,0,-1000};
 		int n=arr.length;
-		int minI,minJ,maxI,maxJ=0;
-		minI=minJ=999999;
-		maxI=maxJ=-999999;
-		int mid=0;
-		if(n%2==0) mid=n/2; else mid=n/2+1;
-		for(int i=0,j=mid ; i<mid ; i++) {
-			maxI=Math.max(maxI, arr[i]);
-			maxJ=Math.max(maxJ, arr[j]);
-			
-			minI=Math.min(minI, arr[i]);
-			minJ=Math.min(minJ, arr[j]);
-			if(j<n-1)
-				j++;
-			
-		}
-		System.out.println("Maximum Element in an array= "+Math.max(maxI, maxJ));
-		System.out.println("Minimum Element in an array= "+Math.min(minI, minJ));
+		MinMaxe mm=getMinMax(arr,n);
+		
+		System.out.println("Maximum Element in an array= "+mm.max);
+		System.out.println("Minimum Element in an array= "+mm.min);
+	}
+	
+	public static MinMaxe getMinMax(int arr[],int n){
+	    int maxI;
+	    int minI;
+	    int maxJ;
+	    int minJ;
+	    maxI=maxJ=-999999;
+	    minI=minJ=999999;
+	    
+	    int mid=n/2;
+	    if(n%2!=0)// if odd
+	        mid+=1;
+	        
+	    for(int i=0,j=mid; i<mid; i++){
+	        maxI=Math.max(maxI,arr[i]);
+	        maxJ=Math.max(maxJ,arr[j]);
+	        
+	        minI=Math.min(minI,arr[i]);
+	        minJ=Math.min(minJ,arr[j]);
+	        if(j<n-1) j++;
+	    }
+	    MinMaxe obj=new MinMaxe();
+	    obj.max=Math.max(maxI,maxJ);
+	    obj.min=Math.min(minI,minJ);
+	    return obj;
 	}
 
 }
+class MinMaxe{
+    int max;
+    int min;
+    public MinMaxe(){
+        max=-999999;
+        min=999999;
+    }
+    
+}    
--- 02.  Algorithms/01. Arrays/02. MaxMin/README.md
+++ 02.  Algorithms/01. Arrays/02. MaxMin/README.md
@@ -1,3 +1,27 @@
 ![CamScanner 06-03-2021 23 31 35_2](https://user-images.githubusercontent.com/71629248/120780516-d6acdb80-c545-11eb-944c-d640fd0b8f14.jpg)
 ![CamScanner 06-03-2021 23 31 35_3](https://user-images.githubusercontent.com/71629248/120779917-335bc680-c545-11eb-8c3b-245d22e4af84.jpg)
+```java
+public static void main(String[] args) {
+		//            0  1  2    3  4    5  6
+		int arr[]= {-900,23,67,1200,34,9800,0};
+		int n=arr.length;
+		int minI,minJ,maxI,maxJ=0;
+		minI=minJ=999999;
+		maxI=maxJ=-999999;
+		int mid=0;
+		if(n%2==0) mid=n/2; else mid=n/2+1;
+		for(int i=0,j=mid ; i<mid ; i++) {
+			maxI=Math.max(maxI, arr[i]);
+			maxJ=Math.max(maxJ, arr[j]);
+			
+			minI=Math.min(minI, arr[i]);
+			minJ=Math.min(minJ, arr[j]);
+			if(j<n-1)
+				j++;
+			
+		}
+		System.out.println("Maximum Element in an array= "+Math.max(maxI, maxJ));
+		System.out.println("Minimum Element in an array= "+Math.min(minI, minJ));
+	}
+```
 # **[See my code here](./MinMax.java)**
