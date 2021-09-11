5c585bcc0576acdb900b79d03b30ae376dca7e2d
Vikash Gupta <heyimvikash@gmail.com>
2021-09-11 15:38:19 +0530

    Update DNF012.java

--- 02.  Algorithms/01. Arrays/05. DNF012/DNF012.java
+++ 02.  Algorithms/01. Arrays/05. DNF012/DNF012.java
@@ -7,27 +7,27 @@ public class DNF012 {
 	public static void main(String[] args) {
 		int arr[]={0, 2, 1, 2, 0,1,2,0,2};
 		int n=arr.length;
-		
-		 int low=0;int mid=0;int high=n-1;
-	      while(mid<=high){
-	          switch(arr[mid]){
-	              case 0: swap(arr,low,mid);
-	              		  mid++;
-	              		  low++;
+
+		 int i=0; int curr=0; int k=n-1;
+	      while(curr<=k){
+	          switch(arr[curr]){
+	              case 0: swap(arr,i,curr);
+	              		  curr++;
+	              		  i++;
 	              break;
-	              case 1: mid++;
+	              case 1: curr++;
 	              break;
-	              case 2: swap(arr,high,mid);
-	              		  high--;
-	              break;  
+	              case 2: swap(arr,k,curr);
+	              		  k--;
+	              break;
 	          }
 	      }
 	      System.out.println(Arrays.toString(arr));
 	}
 	public static void swap(int a[],int i,int j){
-        int temp=a[i];
-        a[i]=a[j];
-        a[j]=temp;
-    }
+		int temp=a[i];
+		a[i]=a[j];
+		a[j]=temp;
+    	}
 
 }
